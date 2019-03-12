b0f50a670324b70253a7856500ca66da54f316e1
mathildebuenerd <mathilde.buenerd@gmail.com>
2019-03-12 17:11:43 +0100

    set up a timer : notification disappears after x seconds

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ A simple notification widget made with React.
 ## About
 Setup following the [TypeScript React Starter](https://github.com/Microsoft/TypeScript-React-Starter).
 
-## Tools 
+## Tools & Ressources
 - [React Inspector for Firefox](https://addons.mozilla.org/en-US/firefox/addon/react-devtools/)
+- [Lifecycle methods](https://reactjs.org/docs/state-and-lifecycle.html) like `componentDidMount()` and `componentWillUnmount()`
--- src/components/NotificationWidget.tsx
+++ src/components/NotificationWidget.tsx
@@ -5,17 +5,34 @@ export interface Props {
     message: string;
     position: string;
     type: string;
+    duration?: number;
+
 }
 
-class NotificationWidget extends React.Component<Props, object> {
-    render() {
-        const {message, position, type} = this.props;
+interface State {
+    expired: boolean;
+}
 
-        return this.show(message, position, type);
+class NotificationWidget extends React.Component<Props, State> {
 
+    // Create a variable for storing the setTimeout for expiration
+    expiration: any;
+
+    // Setting default duration time
+    // https://stackoverflow.com/questions/37282159/default-property-value-in-react-component-using-typescript
+    public static defaultProps = {
+        duration: 3000
     }
 
-    show(message: string, position: string, type: string) {
+    constructor(props: Props) {
+        super(props);
+        this.state = {expired: false};
+    }
+
+
+
+    show(message: string, position: string, type: string): object {
+
         return (
             <div className="notification">
                 <span>Message: {message}</span>
@@ -23,6 +40,25 @@ class NotificationWidget extends React.Component<Props, object> {
                 <span>Type: {type}</span>
             </div>
         )
+    }
+
+    componentDidMount(): void {
+        // In React, setting up a timer whenever the component is rendered to the DOM for the first time is called "mounting"
+        this.expiration = setTimeout( () => {
+            this.unmountComponent();
+        }, this.props.duration);
+    }
+
+    unmountComponent(): void {
+        // clearing a timer previously setted with componentDidMound() is called "unmounting"
+        this.setState({expired: true});
+        clearTimeout(this.expiration);
+    }
+
+    render() {
+        // If the notification state is not expired, we show it
+        // If it is, we return an empty element ''
+        return this.state.expired ? '' : this.show(this.props.message, this.props.position, this.props.type);
     }
 
 }
